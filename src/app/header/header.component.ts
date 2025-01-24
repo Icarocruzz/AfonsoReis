3b8f97c02e3540e1ffc03f3f38c26cee03967fb3
Icarocruzz <5kandradee@gmail.com>
2025-01-24 16:10:32 -0300

    feat: responsividade e aplicação do menu lateral, e icones corretos para o menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,5 +10,16 @@ import {NgOptimizedImage} from '@angular/common';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+menuValue:boolean = false;
+menu_icon:string = 'bi bi-list'
 
+openMenu () {
+  this.menuValue = !this.menuValue;
+  this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+}
+
+closeMenu () {
+  this.menuValue = false;
+  this.menu_icon = 'bi bi-list';
+}
 }
